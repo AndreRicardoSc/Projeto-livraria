5340820a83590b96264ac498b7c92f3dec686e24
AndreRicardoSc <andrenarutto7@gmail.com>
2025-04-15 21:39:19 -0300

    publish branch

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,28 @@
+<script setup></script>
+
+<template>
+  <div class="footer">
+    <div>
+        <h1>IFbooks</h1>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+    <div class="direitos">
+        <p>
+            &copy; Alguns direitos reservados. IFbooks 2025.
+        </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+*{
+    color: #fff;
+}
+div.footer{
+    background: #27AE60;
+}
+</style>
