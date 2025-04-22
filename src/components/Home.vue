871f328806634052dfefe715b0b5cc667d624fb2
AndreRicardoSc <andrenarutto7@gmail.com>
2025-04-22 15:54:14 -0300

    FEAT: Ajustes finais e adição dos icones como svg #2

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -72,7 +72,10 @@ const produtos = ref([
           <div class="info">
             <p>R${{ item.preco }}</p>
           </div>
-          <button>Comprar</button>
+          <button>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.<path d="M0 24C0 10.7 10.7 0 24 0L69.5 0c22 0 41.5 12.8 50.6 32l411 0c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3l-288.5 0 5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5L488 336c13.3 0 24 10.7 24 24s-10.7 24-24 24l-288.3 0c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5L24 48C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg>
+            Comprar
+          </button>
         </li>
       </ul>
     </section>
@@ -80,6 +83,10 @@ const produtos = ref([
 </template>
 
 <style scoped>
+svg{
+  width: 20px;
+  fill: #fff;
+}
 * {
   color: #000;
 }
@@ -87,12 +94,12 @@ button {
   cursor: pointer;
 }
 section.lancamentos {
-  margin: 4vw 2vw;
+  margin: 4vw 8vw;
 }
 section.lancamentos h2 {
   margin: 4vw;
-  font-size: 40px;
-  font-weight: 300;
+  font-size: 4rem;
+  font-weight: 400;
 }
 section.lancamentos ul {
   display: flex;
@@ -101,11 +108,14 @@ section.lancamentos ul {
   flex-wrap: wrap;
 }
 section.lancamentos ul li {
-  width: 25%;
+  width: 23%;
   list-style: none;
-  margin: 2vw 0;
+  margin: 2vw 2px;
   padding: 0;
   font-size: 130%;
+}
+section.lancamentos ul li img{
+  width: 100%;
 }
 section.lancamentos ul li p{
     margin: 1vw 0;
@@ -116,8 +126,11 @@ section.lancamentos ul li p.autor{
 section.lancamentos ul li div.info p{
     font-size: 130%;
 }
+section.lancamentos ul li div.button{
+  background-color: #000;
+}
 section.lancamentos ul li button{
-    width: 50%;
+    width: 100%;
     background: #27AE60;
     color: #fff;
     padding: 10px 0;
